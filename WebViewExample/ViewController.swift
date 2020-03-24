8e6dedadb77502dc62df17ffa5d356aa856aefcf
moxDroid Labs Inc <pritam.world@gmail.com>
2020-03-24 18:22:23 -0400

    Webview with initial code and design

--- WebViewExample/ViewController.swift
+++ WebViewExample/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+//Need to use
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myWebKitView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadLambtonUrl()
     }
+    
+    
+    func loadLambtonUrl()
+    {
+        let url = URL(string: "https://www.lambtoncollege.ca/")
+        let urlReq = URLRequest(url: url!)
+        myWebKitView.load(urlReq)
+    }
+    
+    func loadHtmlString()
+    {
+        let htmlString = "<h1>Hello World</h1>"
+        myWebKitView.loadHTMLString(htmlString, baseURL: nil)
+        
+    }
+    
+    
 
-
+    @IBAction func btnNavigation(_ sender: UIBarButtonItem)
+    {
+        switch sender.tag {
+        case 0://Home Button is pressed
+            myWebKitView.reloadFromOrigin()
+        case 1://Prev Button is pressed
+            if myWebKitView.canGoBack
+            {
+                print("Go back")
+                myWebKitView.goBack()
+            }else
+            {
+                print("Can't Go back")
+            }
+        default:
+            print("No Navigation action found...")
+        }
+    }
+    
 }
 
